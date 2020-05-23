0a96067ea51b8da6ae3be0de34da207eb1d07d9d
Lalit Narayan Mudgal <lalitrpvv@gmail.com>
2020-05-23 19:31:28 +0530

    added sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,28 @@ function ScrollToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+function SidebarToggle() {
+    var navbar = document.getElementById('sidebar');
+    if (navbar.style.left == '0px') {
+        navbar.style.left = '-250px';
+        var toggle = document.getElementsByClassName('fa-arrow-left')[0];
+        toggle.className = '';
+        toggle.className += 'fa fa-arrow-right';
+    }
+    else {
+        navbar.style.left = '0px';
+        var toggle = document.getElementsByClassName('fa-arrow-right')[0];
+        toggle.className = '';
+        toggle.className += 'fa fa-arrow-left';
+    }
+}
+function HideNav() {
+    var navbar = document.getElementById('sidebar');
+    if (window.pageYOffset > 0) {
+        navbar.style.left = '-250px';
+
+        var toggle = document.getElementsByClassName('fa-arrow-left')[0];
+        toggle.className = '';
+        toggle.className += 'fa fa-arrow-right';
+    }
+}
